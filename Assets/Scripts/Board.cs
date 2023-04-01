b47b0f65b2fb27f72fb585e2654ee7bae7e19e7e
Lautaro Cepeda <LautaroCepeda.Developer@gmail.com>
2023-04-01 16:01:27 -0300

    Added properties to camera
    Properties added to modify the position of the camera at convenience (interface)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,15 +7,17 @@ public class Board : MonoBehaviour
 {
     #region Variables (REGION)
 
-    [SerializeField] int width, height;
-    [SerializeField] GameObject tileObject;
+    [SerializeField] int width, height; //Size of the board (Each unit, represents one object)
+    [SerializeField] GameObject tileObject; //Object to fill the board
+    [SerializeField] float cameraSizeOffset, cameraVerticalOffset; // [0]OrthographicSize, [1]cameraVerticalPosition
 
     #endregion
 
     #region Functions (REGION)
-    private void SetupBoard()
+
+    private void SetupBoard() //Set the grid
     {
-        for(int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
@@ -24,12 +26,28 @@ public class Board : MonoBehaviour
             }
         }
     }
+
+    private void PositionCamera()
+    {
+        float newPosX = (float)width / 2f;
+        float newPosY = (float)height / 2f;
+
+        //Moving the camera to correctly position the elements
+        Camera.main.transform.position = new Vector3(newPosX - 0.5f, newPosY - 0.5f + cameraVerticalOffset, -10);
+
+        float horizontal = width + 1;
+        float vertical = (height / 2) + 1;
+
+        //Adapting the camera based on the grid
+        Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset: vertical + cameraVerticalOffset; 
+    }
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
+        PositionCamera();
     }
 
 
